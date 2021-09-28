8305663dafa455984df44e17e74f3552cd0be868
yoshi0805 <90741509+yoshi0805@users.noreply.github.com>
2021-09-28 16:19:57 +0900

    deviseに関するビューファイルの配置

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_053549) do
+ActiveRecord::Schema.define(version: 2021_09_28_071058) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
